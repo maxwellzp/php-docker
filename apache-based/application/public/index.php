59d4636e382995bd234081e94faa2d39623d7d5b
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-03-12 14:18:00 +0200

    Modify docker files and show usage in PHP code

--- apache-based/application/public/index.php
+++ apache-based/application/public/index.php
@@ -1,5 +1,29 @@
 <?php
 
-echo 'PHP + Docker + Apache2 + MariaDB';
+echo 'PHP + Docker + Apache2 + MariaDB' . '<br>';
 
-phpinfo();
+$dsn = sprintf("mysql:host=%s;dbname=%s;charset=UTF8",
+    getEnv('DB_HOST'), getEnv('DB_DATABASE')
+);
+
+try {
+    $pdo = new PDO($dsn, getEnv('DB_USER'), getEnv('DB_PASSWORD'));
+
+    if ($pdo instanceof PDO) {
+        echo "Connected to DB successfully!" . '<br>';
+    }
+} catch (PDOException $e) {
+    echo sprintf("DB exception: %s", $e->getMessage());
+}
+
+try {
+    $redis = new Redis([
+        'host' => getEnv('REDIS_HOST'),
+        'port' => (int)getEnv('REDIS_PORT'),
+    ]);
+    $redis->set('foo', 'bar');
+    echo 'redis value: ' . $redis->get('foo') . '<br>';
+} catch (Exception $e) {
+    echo sprintf("Redis exception: %s", $e->getMessage());
+}
+
--- nginx-based/application/public/index.php
+++ nginx-based/application/public/index.php
@@ -1,6 +1,34 @@
 <?php
 
-echo 'PHP-FPM + Docker + Nginx + MariaDB';
+echo 'PHP-FPM + Docker + Nginx + Postgres' . '<br>';
 
-phpinfo();
+//echo '<pre>';
+//var_dump($_ENV);
+//echo '</pre>';
+
+
+$dsn = sprintf("pgsql:host=%s;port=5432;dbname=%s",
+    getEnv('DB_HOST'), getEnv('DB_DATABASE')
+);
+
+try {
+    $pdo = new PDO($dsn, getEnv('DB_USER'), getEnv('DB_PASSWORD'));
+
+    if ($pdo instanceof PDO) {
+        echo "Connected to DB successfully!" . '<br>';
+    }
+} catch (PDOException $e) {
+    echo sprintf("DB exception: %s", $e->getMessage()) . '<br>';
+}
+
+try {
+    $redis = new Redis([
+        'host' => getEnv('REDIS_HOST'),
+        'port' => (int)getEnv('REDIS_PORT'),
+    ]);
+    $redis->set('foo', 'bar');
+    echo 'redis value: ' . $redis->get('foo') . '<br>';
+} catch (Exception $e) {
+    echo sprintf("Redis exception: %s", $e->getMessage());
+}
 
